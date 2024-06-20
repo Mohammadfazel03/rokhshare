5dc7464e0ef2ca2435c0faaf52b113cc8afab4c3
mohammad <mohamadfazel1382@gmail.com>
2024-06-20 12:23:04 +0330

    [bug] fixed bug download media

--- rokhshare/settings.py
+++ rokhshare/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 
 from pathlib import Path
@@ -158,3 +159,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
 }
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
